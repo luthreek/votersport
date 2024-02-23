82927fdfc0a2947d21cd351ae5380be510aed5ca
luthreek <iliashestakov@mail.ru>
2024-02-23 12:48:02 +0300

    Update StakePool.sol

--- src/StakePool.sol
+++ src/StakePool.sol
@@ -4,9 +4,10 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
-import "@openzeppelin/contracts/access/Ownable2Step.sol"
-import "@openzeppelin/contracts//utils/Pausable.sol"
-contract TokenStaking is SafeERC20,Ownable2Step,Pausable{
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Pausable.sol";
+contract TokenStaking is Ownable,Pausable{
+    using SafeERC20 for IERC20;
     using Math for uint256;
     struct Stake {
         uint256 amount;
@@ -22,8 +23,7 @@ contract TokenStaking is SafeERC20,Ownable2Step,Pausable{
 
     event Staked(address indexed staker, uint256 amount);
     event Unstaked(address indexed staker, uint256 amount, uint256 profit);
-
-    constructor(IERC20 _token, uint256 _stakingDuration,uint256 _profitPercentage) {
+    constructor(IERC20 _token, uint256 _stakingDuration,uint256 _profitPercentage,address _owner) Ownable(_owner) {
         token = _token;
         stakingDuration = _stakingDuration;
         profitPercentage=_profitPercentage;
@@ -32,28 +32,25 @@ contract TokenStaking is SafeERC20,Ownable2Step,Pausable{
     function stake(uint256 _amount) external whenNotPaused{
         require(_amount > 0, "Amount must be greater than 0");
         require(stakes[msg.sender].amount == 0, "Already staked");
-        require(_profitPercentage <= 100, "Profit percentage must be less than or equal to 100");
-        stakes[msg.sender] = Stake(_amount, block.timestamp, _profitPercentage);
-        require(safeTransferFrom(token,msg.sender, address(this), _amount), "Transfer failed");
-
-        
+        stakes[msg.sender] = Stake(_amount, block.timestamp, profitPercentage);
+        IERC20(token).safeTransferFrom(msg.sender, address(this), _amount);
         emit Staked(msg.sender, _amount);
     }
 
     function unstake() external whenNotPaused {
         require(stakes[msg.sender].amount > 0, "No stake");
-        require(block.timestamp >= stakes[msg.sender].startTime.add(stakingDuration), "Staking duration not passed");
+        require(block.timestamp >= stakes[msg.sender].startTime + stakingDuration, "Staking duration not passed");
 
         uint256 stakedAmount = stakes[msg.sender].amount;
         delete stakes[msg.sender];
 
-        uint256 profit = stakedAmount.mul(profitPercentage).div(100);
+        uint256 profit = stakedAmount*profitPercentage / 100;
 
-        uint256 totalAmount = stakedAmount.add(profit);
+        uint256 totalAmount = stakedAmount+profit;
         uint256 tokenBalance = token.balanceOf(address(this));
         uint256 amountToReturn = totalAmount > tokenBalance ? tokenBalance : totalAmount;
 
-        require(safeTransfer(token,msg.sender, amountToReturn), "Transfer failed");
+        IERC20(token).safeTransfer(msg.sender, amountToReturn);
 
         emit Unstaked(msg.sender, stakedAmount, profit);
     }
@@ -61,10 +58,14 @@ contract TokenStaking is SafeERC20,Ownable2Step,Pausable{
         require(_newProfitPercentage <= 100, "Profit percentage must be less than or equal to 100");
         profitPercentage = _newProfitPercentage;
     }
-    function _pause() internal virtual override onlyOwner {
-        _paused = (!_paused);
-    }
-    function getStake(address _address) external view returns (uint256, uint256, uint256) whenNotPaused {
+    function SwitchPaused() external onlyOwner{
+        if (paused()) {
+            _unpause();
+        }else {
+            _pause();
+        }
+    }   
+    function getStake(address _address) external view whenNotPaused returns (uint256, uint256, uint256) {
         return (stakes[_address].amount, stakes[_address].startTime, stakes[_address].profitPercentage);
     }
 }
